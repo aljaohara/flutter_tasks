e418b138b299f3cc7de5cb99e92818674e995f9e
jejea <aljoharalfassam@gmail.com>
2023-08-08 05:57:08 +0300

    my first project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'dart:html';
+
 
 import 'package:flutter/material.dart';
 
@@ -15,37 +15,126 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         title: 'Flutter Demo',
         home: Scaffold(
-          backgroundColor: Colors.green,
+          backgroundColor: Colors.white,
           appBar: AppBar(
-            title: Text('Jeje')
+            title: const Text('Jeje'),
+            backgroundColor: Colors.purple
             , centerTitle: true,
-            leading: Icon(Icons.icecream_rounded, color: Colors.yellow),
           ),
 
-          body: Center(
-            child: TextButton(
-              onPressed: () {
-                print("This is my first project");
-              },
-              style: ButtonStyle(
-                backgroundColor:
-                MaterialStateProperty.all(Colors.yellow),
-              ),
-              child: const Text(
-                'Click Me !',
+           body: Column(
+             children: [
+                   Padding( padding: const EdgeInsets.symmetric(
+                     vertical: 25.0,
+                   ),
+              child: Row(
+                     children: [
+                       Expanded(
+              child: TextButton
+                         (onPressed:  (){
+                         print('im clicked');
+                       },
+                         style: ButtonStyle (backgroundColor: MaterialStateProperty.all(Colors.yellow) ),
+                         child: const  Text('Click Me !'),
+                       ),
+                       ),
+                       Expanded(child: TextButton
+                         (onPressed:  (){
+                         print('im clicked');
+                       },
+                         style: ButtonStyle (backgroundColor: MaterialStateProperty.all(Colors.yellow) ),
+                         child: const  Text('Click Me !'),
+                       ),
+                       ),
+                       Expanded(
+                         child: TextButton
+                           (onPressed:  (){
+                           print('im clicked');
+                         },
+                           style: ButtonStyle (backgroundColor: MaterialStateProperty.all(Colors.yellow) ),
+                           child: const  Text('Click Me !'),
+                         ),
+                       )
+                     ],
+               ),
 
-                style: TextStyle(
-                  color: Colors.blue,
-                  fontSize: 16,
-                ),
-              ),
+                   ),
+                 const Row(children: [
+                     Icon(Icons.home, size: 120, color: Colors.purple,),
+                     Expanded(child:
+                     Text('Hello i am trying all cases to test all cases',
+                       style:
+                       TextStyle(fontSize: 25 ),
+                     ),
+                     ),
+               ],
+               ),
 
-            ),
-
-          ),
-
-        )
-    );
+                     const SingleChildScrollView(
+                       scrollDirection: Axis.horizontal,
+                       child:  Row(children: [
+                            Icon(Icons.settings, size: 50, color: Colors.green,),
+                            Icon(Icons.settings, size: 50, color: Colors.green,),
+                            Icon(Icons.settings, size: 50, color: Colors.green,),
+                            Icon(Icons.settings, size: 50, color: Colors.green,),
+                            Icon(Icons.settings, size: 50, color: Colors.green,),
+                            Icon(Icons.settings, size: 50, color: Colors.green,),
+                            Icon(Icons.settings, size: 50, color: Colors.green,),
+                            Icon(Icons.settings, size: 50, color: Colors.green,),
+                            Icon(Icons.settings, size: 50, color: Colors.green,),
+                         ],),
+                     ),
+                     const Expanded(
+                       child: SingleChildScrollView(
+                       child:  SizedBox(
+                         width: double.infinity,
+                         child:  Column(
+                           crossAxisAlignment: CrossAxisAlignment.start,
+                           children: [
+                           Text('Hello my name is aljaohara',
+                             style: TextStyle(fontSize: 30 ),),
+                           Text('Hello my name is aljaohara',
+                             style: TextStyle(fontSize: 30 ),) ,
+                           Text('Hello my name is aljaohara',
+                             style: TextStyle(fontSize: 30 ),) ,
+                           Text('Hello my name is aljaohara',
+                             style: TextStyle(fontSize: 30 ),) ,
+                           Text('Hello my name is aljaohara',
+                             style: TextStyle(fontSize: 30 ),) ,
+                           Text('Hello my name is aljaohara',
+                             style: TextStyle(fontSize: 30 ),) ,
+                           Text('Hello my name is aljaohara',
+                             style: TextStyle(fontSize: 30
+                             ),
+                           ) ,Text('Hello my name is aljaohara',
+                             style: TextStyle(fontSize: 30
+                             ),
+                           ) ,Text('Hello my name is aljaohara',
+                             style: TextStyle(fontSize: 30
+                             ),
+                           ) ,Text('Hello my name is aljaohara',
+                             style: TextStyle(fontSize: 30
+                             ),
+                           ) ,Text('Hello my name is aljaohara',
+                             style: TextStyle(fontSize: 30
+                             ),
+                           ) ,Text('Hello my name is aljaohara',
+                             style: TextStyle(fontSize: 30
+                             ),
+                           ) ,Text('Hello my name is aljaohara',
+                             style: TextStyle(fontSize: 30
+                             ),
+                           ) ,Text('Hello my name is aljaohara',
+                             style: TextStyle(fontSize: 30
+                             ),
+                           ) ,
+                         ],),
+                       ),
+                      ),
+                     ),
+                 ],),
+             ),
+           );
   }
 }
 
